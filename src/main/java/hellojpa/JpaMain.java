6007172a10f8a9694144699814954e958bcddbf3
MinseokGo <rhalstjr1999@naver.com>
2024-08-21 00:01:49 +0900

    🚀 feat: entity manager

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,16 +1,32 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
 
-        em.close();
-        emf.close();
+        try {
+//            Member member = entityManager.find(Member.class, 1L);
+//            member.setName("HelloJPA");
+            List<Member> members = entityManager.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+            members.forEach(member -> System.out.println(member.getName()));
+
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+        entityManagerFactory.close();
     }
 }
